7a507a2065a621eba0cbed485900d0df7a8769d7
scarf <greenscarf005@gmail.com>
2022-03-29 23:52:25 +0900

    feat: :sparkles: test util library

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 #include "Fixed.hpp"
+#include "test.hpp"
+
+void testDefault() {
+  test::header("Mandatory");
+  Fixed a;
+  Fixed b(a);
+  Fixed c;
+
+  c = b;
+
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+}
+
+void testCustom() {
+  test::header("Custom");
+  {
+    test::subject("Default Constructor is 0");
+    TEST_EXPECT(Fixed().getRawBits(), 0);
+  }
+  {
+    test::subject("Copy Constructor Works");
+    Fixed a;
+    a.setRawBits(42);
+    Fixed b(a);
+    TEST_EXPECT(b.getRawBits(), a.getRawBits());
+  }
+}
 
 int main(void) {
-  {
-    Fixed a;
-    Fixed b(a);
-    Fixed c;
-
-    c = b;
-
-    std::cout << a.getRawBits() << std::endl;
-    std::cout << b.getRawBits() << std::endl;
-    std::cout << c.getRawBits() << std::endl;
-  }
+  testDefault();
+  testCustom();
   return 0;
 }
--- ex00/util/test.hpp
+++ ex00/util/test.hpp
@@ -1,22 +1,50 @@
 #ifndef __TEST_H__
 #define __TEST_H__
 
+#include <cassert>
+#include <cstdlib>
 #include <iostream>
+#include <string>
 #include "color.hpp"
 
 using std::cout;
+using std::string;
+
+// Macros
+#define TEST_EXPECT(expected, actual) \
+  test::expect(#expected " == " #actual, expected, actual)
+
+#define TEST_LOG(x) test::log(#x, x)
 
 namespace test {
 
 template <typename T>
 void header(T const& t) {
-  cout << "[[" << t << ":\n";
+  cout << BHYEL << string(80, '-') << "\n";
+  cout << "[[[ Testing " BBLU << t << BHYEL " ]]]\n" END;
 }
 
 template <typename T>
-void logln(T value) {
-  cout << value << "\n";
+void subject(T const& t) {
+  cout << MAG "\n[ case " BCYN << t << MAG " ]\n" END;
 }
 
+template <typename T>
+void log(const string& str, T value) {
+  cout << BLU << str << HYEL ": " BCYN << value << "\n";
+}
+
+template <typename T>
+void expect(const string& name, T const& expected, T const& actual) {
+  cout << BLU << name << HYEL ": ";
+  if (expected == actual) {
+    cout << BGRN << "OK\n" END;
+  } else {
+    cout << BRED << "KO\n";
+    cout << " expected: " << expected << " actual: " << actual << END "\n";
+    std::exit(EXIT_FAILURE);
+  }
+}
 }  // namespace test
+
 #endif  // __TEST_H__
